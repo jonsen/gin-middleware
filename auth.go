377159b845cc18c89caa919a2f3c63f0a8c00499
Jonsen Yang <im16hot@gmail.com>
2018-11-21 19:50:42 +0800

    fix AuthRequest() not abort request

--- auth.go
+++ auth.go
@@ -36,10 +36,10 @@ func AuthRequest(disUrls ...string) gin.HandlerFunc {
 			}
 		}
 
-		if !match && c.Request.RequestURI != "/login" && c.Request.RequestURI != "/users/logined" {
+		if !match && c.Request.RequestURI != "/login" {
+			c.Abort()
 			c.Redirect(302, disUrls[0])
 		}
 
-		c.Next()
 	}
 }
